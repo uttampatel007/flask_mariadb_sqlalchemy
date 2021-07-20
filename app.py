c2790accf151a22316eeabb31e7f304e8d1113c5
uttampatel007 <uttam11.velani11@gmail.com>
2021-07-20 16:15:05 +0530

    foreign-key-relation-added

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ Referance : https://docs.sqlalchemy.org/en/14/orm/tutorial.html
 import sqlalchemy
 from sqlalchemy.sql import func
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Table, MetaData, Text
 
-engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root:smart@127.0.0.1:3306/flask_mariadb_sqlalchemy")
+
 # Engines are used to manage two crucial factors: Pools and Dialects. 
+engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root:smart@127.0.0.1:3306/flask_mariadb_sqlalchemy")
+
 
 Base = declarative_base()
+
 
 class Employee(Base):
     __tablename__ = 'employeesK'
@@ -19,20 +23,24 @@ class Employee(Base):
     active = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
     created_at = sqlalchemy.Column(sqlalchemy.DateTime, server_default=func.now())
 
-class EmployeeTable(Base):
-    __tablename__ = 'employee_table'
+
+class EmployeeAddress(Base):
+    __tablename__ = 'employee_address_2'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    first_name = sqlalchemy.Column(sqlalchemy.String(length=100))
+    employee_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('employeesK.id'))
     address = sqlalchemy.Column(sqlalchemy.String(length=100))
-    active = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
 
-class EmployeeData(Base):
-    __tablename__ = 'employee_data'
+
+class EmployeeInteger(Base):
+    __tablename__ = 'employee_integer'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    first_name = sqlalchemy.Column(sqlalchemy.String(length=100))
-    data = sqlalchemy.Column(sqlalchemy.String(length=100))
-    active = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+    des = sqlalchemy.Column(sqlalchemy.Integer)
 
+
+class EmployeeText(Base):
+    __tablename__ = 'employee_text'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    des = sqlalchemy.Column(Text)
 
 
 Base.metadata.create_all(engine)
@@ -44,12 +52,15 @@ session = Session()
 
 '''
 # adding one employee
-'''
-
 newEmployee = Employee(first_name="sdsRob", last_name="Hsddb", active=False)
 session.add(newEmployee)
 session.commit()
 
+NewEmployeeAddress = EmployeeAddress(employee_id=newEmployee.id, address='dekkdjkjdejk')
+session.add(NewEmployeeAddress)
+session.commit()
+
+'''
 
 '''
 # adding multiple 
